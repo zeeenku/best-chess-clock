08b9d7b97d79ee19b52dad620cf119aeba804635
zeeenku <enajjachi.zakariaa@gmail.com>
2025-01-20 22:02:35 +0100

    config prototype added

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -22,7 +22,7 @@ type Player = {
   addiTime: number,
 };
 
-type starTimeRecom = {
+type TimeRecom = {
   name: string,
   time : number,
 };
@@ -32,7 +32,7 @@ function App() {
   const [isSamePlayTime, setSamePlayTime] = useState(true);
   const startTime = 10; const addiTime = 5;
 
-  const startTimeRecom : starTimeRecom[] = [
+  const startTimeRecom : TimeRecom[] = [
     {
       name : "5m",
       time : 5,
@@ -70,6 +70,46 @@ function App() {
       time : 120,
     },
   ];
+
+
+  const addiTimeRecom : TimeRecom[] = [
+    {
+      name : "0s",
+      time : 0,
+    },
+
+    {
+      name : "1s",
+      time : 1,
+    },
+
+    {
+      name : "3s",
+      time : 3,
+    },
+
+
+    {
+      name : "5s",
+      time : 5,
+    },
+
+    {
+      name : "10s",
+      time : 10,
+    },
+
+
+    {
+      name : "30s",
+      time : 30,
+    },
+
+    {
+      name : "1m",
+      time : 60,
+    },
+  ];
   const ps : Player[] = [
     {
       startTime: startTime,
@@ -85,79 +125,43 @@ function App() {
   const [players, setPlayers] = useState(ps);
 
 
-  const getPlayerNode = (player: Player, showTitle : boolean) => {
-    return (
-    <div key={`player-${player.id}`} className="flex flex-col gap-y-2 my-4">
-      {
-        showTitle ? (
-          <>
-            <h2>player {player.id}</h2>
-          </>
-        ) : (<></>)
-      }
-      
-      <hr className="block"/>
-      
-      <div className="flex w-full justify-center items-center space-x-2">
-        {
-          startTimeRecom.map(el=>
-            <Button 
-            className={`${el.time == player.startTime ? "bg-amber-950" : "bg-amber-900" }  hover:bg-amber-950 w-full`} 
-            onClick={()=>setPlayTime(player.id, el.time)}>{el.name}</Button>
-          )
-        }
-
-        </div>
-        <div className="flex w-full items-center space-x-2">
-          <Label htmlFor="start-time" className="text-gray-100 w-28">start time</Label>
-          <Input onChange={()=>{}} type="start-time" value={player.startTime} className="bg-slate-100 text-slate-900" placeholder="Minutes" />
-          <Button className="bg-amber-900 hover:bg-amber-950" onClick={()=>setPlayTime(player.id, player.startTime+1)}>+</Button>
-          <Button className="bg-amber-900 hover:bg-amber-950" onClick={()=>setPlayTime(player.id, player.startTime-1)}>-</Button>
-        </div>
-
-
-        {/* <div className="flex w-full items-center space-x-2">
-          <Label htmlFor="addit-time" className="text-gray-100 w-28">addi. time</Label>
-          <Input type="addit-time" className="bg-slate-100 text-slate-900" placeholder="Seconds" />
-          <Button className="bg-amber-900 hover:bg-amber-950" onClick={()=>{}}>+</Button>
-          <Button className="bg-amber-900 hover:bg-amber-950" onClick={()=>{}}>-</Button>
-        </div> */}
-        </div>
-    );
-  }
 
   const setPlayTime = (playerId: number, startTime: number) => {
 
-    console.log("tt")
     if(startTime < 1 || startTime > 3*60){
       return;
     }
 
 
-    const newPlayers = players;
-    newPlayers.map((el)=>{
+    const newPlayers = players.map((el)=>{
       if(el.id == playerId){
         el.startTime = startTime;
       }
       return el;
     })
-    setPlayers(players);
-    console.log("tt")
+    setPlayers(newPlayers);
   }
 
   useEffect(()=>{
     
-  },[isSamePlayTime])
+  },[isSamePlayTime, players])
 
-  const setaddiTime = (playerIndex: number, addiTime: number) => {
-    const newPlayers = players;
-    newPlayers.map((el, index)=>{
-      if(index == playerIndex){
+  const setAddiTime =  (playerId: number, addiTime: number) => {
+
+    if(addiTime < 0 || addiTime > 60*2){
+      return;
+    }
+
+
+    const newPlayers = players.map((el)=>{
+      if(el.id == playerId){
         el.addiTime = addiTime;
       }
+      return el;
     })
-    setPlayers(players);
+    setPlayers(newPlayers);
   }
+
 
   return (
     <>
@@ -179,39 +183,58 @@ function App() {
 
         <div >
 
-      {isSamePlayTime ? (
-      <>hhh</>
-    ) : 
-      players.map((el)=> getPlayerNode(el, true))
-    }
-          <h2>player 1</h2>
-          <hr className="block"/>
+      
+      {
+          (isSamePlayTime ? [players[0]] : players).map(player =>
+          <div key={`player-${player.id}`} className="flex flex-col gap-y-2 my-4">
+            {
+              !isSamePlayTime ? (
+                <>
+                  <h2>player {player.id}</h2>
+                </>
+              ) : (<></>)
+            }
+            
+            <hr className="block"/>
+            
+            <div className="flex w-full justify-center items-center space-x-2">
+              {
+                startTimeRecom.map(el=>
+                  <Button 
+                  className={`${el.time == player.startTime ? "bg-amber-950" : "bg-amber-900" }  hover:bg-amber-950 w-full`} 
+                  onClick={()=>setPlayTime(player.id, el.time)}>{el.name}</Button>
+                )
+              }
+      
+              </div>
+              <div className="flex w-full items-center space-x-2">
+                <Label htmlFor="start-time" className="text-gray-100 w-28">start time</Label>
+                <Input onChange={()=>{}} type="start-time" value={player.startTime} className="bg-slate-100 text-slate-900" placeholder="Minutes" />
+                <Button className="bg-amber-900 hover:bg-amber-950" onClick={()=>setPlayTime(player.id, player.startTime+1)}>+</Button>
+                <Button className="bg-amber-900 hover:bg-amber-950" onClick={()=>setPlayTime(player.id, player.startTime-1)}>-</Button>
+              </div>
+      
+      
 
-        <div className="flex w-full justify-center items-center space-x-2">
-        <Button className="bg-amber-900 hover:bg-amber-950 w-full" onClick={()=>{}}>5</Button>
-        <Button className="bg-amber-900 hover:bg-amber-950 w-full" onClick={()=>{}}>10</Button>
-        <Button className="bg-amber-900 hover:bg-amber-950 w-full" onClick={()=>{}}>15</Button>
-        <Button className="bg-amber-900 hover:bg-amber-950 w-full" onClick={()=>{}}>30</Button>
-        <Button className="bg-amber-900 hover:bg-amber-950 w-full" onClick={()=>{}}>60</Button>
-        <Button className="bg-amber-900 hover:bg-amber-950 w-full" onClick={()=>{}}>90</Button>
-        <Button className="bg-amber-900 hover:bg-amber-950 w-full" onClick={()=>{}}>120</Button>
-
-          </div>
-          <div className="flex w-full items-center space-x-2">
-            <Label htmlFor="start-time" className="text-gray-100 w-28">start time</Label>
-            <Input type="start-time" className="bg-slate-100 text-slate-900" placeholder="Minutes" />
-            <Button className="bg-amber-900 hover:bg-amber-950" onClick={()=>{}}>+</Button>
-            <Button className="bg-amber-900 hover:bg-amber-950" onClick={()=>{}}>-</Button>
-          </div>
-
-
-          <div className="flex w-full items-center space-x-2">
-            <Label htmlFor="addit-time" className="text-gray-100 w-28">addi. time</Label>
-            <Input type="addit-time" className="bg-slate-100 text-slate-900" placeholder="Seconds" />
-            <Button className="bg-amber-900 hover:bg-amber-950" onClick={()=>{}}>+</Button>
-            <Button className="bg-amber-900 hover:bg-amber-950" onClick={()=>{}}>-</Button>
-          </div>
-
+              <div className="flex w-full justify-center items-center space-x-2">
+              {
+                addiTimeRecom.map(el=>
+                  <Button 
+                  className={`${el.time == player.addiTime ? "bg-amber-950" : "bg-amber-900" }  hover:bg-amber-950 w-full`} 
+                  onClick={()=>setAddiTime(player.id, el.time)}>{el.name}</Button>
+                )
+              }
+      
+              </div>
+              <div className="flex w-full items-center space-x-2">
+                <Label htmlFor="addit-time" className="text-gray-100 w-28">addi. time</Label>
+                <Input onChange={()=>{}} value={player.addiTime} type="addit-time" className="bg-slate-100 text-slate-900" placeholder="Seconds" />
+                <Button className="bg-amber-900 hover:bg-amber-950" onClick={()=>setAddiTime(player.id, player.addiTime+1)}>+</Button>
+                <Button className="bg-amber-900 hover:bg-amber-950" onClick={()=>setAddiTime(player.id, player.addiTime-1)}>-</Button>
+              </div>
+              </div>
+        )
+      }
 
         </div>
         <AlertDialogFooter>
