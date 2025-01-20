b16204b62d971371dca0ee25c09f3655579ecb7b
zeeenku <enajjachi.zakariaa@gmail.com>
2025-01-20 20:31:27 +0100

    dialog added

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,12 +1,48 @@
-//import { useState } from 'react'
+import { useState } from 'react'
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog"
+import { Label } from "@/components/ui/label"
+import { Switch } from "@/components/ui/switch"
 
 import './App.css'
 
 function App() {
-  //const [count, setCount] = useState(0)
+  const [isDialogOpen, setDialogOpen] = useState(true);
+  const [isSamePlayTime, setSamePlayTime] = useState(true);
 
   return (
-   <h1>Hello there</h1>
+    <>
+    <h1>hello there</h1>
+      <AlertDialog  open={isDialogOpen}>
+
+      <AlertDialogContent className="bg-slate-900">
+        <AlertDialogHeader>
+          <AlertDialogTitle className="capitalize text-center">The best chess clock</AlertDialogTitle>
+          <AlertDialogDescription>
+            
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+
+        <div className="flex my-4 items-center justify-center space-x-2">
+              <Switch checked={isSamePlayTime} id="airplane-mode" />
+              <Label htmlFor="same-play-time-mode" className="text-gray-100">same playtime</Label>
+        </div>
+
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+    </>
   )
 }
 
