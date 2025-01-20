c77f71cedd79bb6a7fbdb839852f1c0b6cb57faf
zeeenku <enajjachi.zakariaa@gmail.com>
2025-01-20 20:43:31 +0100

    all dialog features added

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -11,6 +11,8 @@ import {
 } from "@/components/ui/alert-dialog"
 import { Label } from "@/components/ui/label"
 import { Switch } from "@/components/ui/switch"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 
 import './App.css'
 
@@ -32,13 +34,37 @@ function App() {
         </AlertDialogHeader>
 
         <div className="flex my-4 items-center justify-center space-x-2">
-              <Switch checked={isSamePlayTime} id="airplane-mode" />
+              <Switch checked={isSamePlayTime} id="same-play-time-mode" />
               <Label htmlFor="same-play-time-mode" className="text-gray-100">same playtime</Label>
         </div>
 
-        <AlertDialogFooter>
-          <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <AlertDialogAction>Continue</AlertDialogAction>
+        <div className="flex flex-col gap-y-2">
+          <div className="flex w-full items-center space-x-2">
+            <Label htmlFor="start-time" className="text-gray-100">start time</Label>
+            <Input type="start-time" className="bg-slate-100 text-slate-900" placeholder="Minutes" />
+            <Button className="bg-amber-900 hover:bg-amber-950" onClick={()=>{}}>+</Button>
+            <Button className="bg-amber-900 hover:bg-amber-950" onClick={()=>{}}>-</Button>
+          </div>
+
+
+          <div className="flex w-full items-center space-x-2">
+            <Label htmlFor="addit-time" className="text-gray-100">addi. time</Label>
+            <Input type="addit-time" className="bg-slate-100 text-slate-900" placeholder="Seconds" />
+            <Button className="bg-amber-900 hover:bg-amber-950" onClick={()=>{}}>+</Button>
+            <Button className="bg-amber-900 hover:bg-amber-950" onClick={()=>{}}>-</Button>
+          </div>
+
+
+        </div>
+        <AlertDialogFooter className="block">
+          <div className="flex w-full justify-between">
+            <div>Developed & Designed By Zenku</div>
+            <AlertDialogAction>start game</AlertDialogAction>
+          </div>
+         
+          <div className="flex">
+            Developer
+          </div>
         </AlertDialogFooter>
       </AlertDialogContent>
     </AlertDialog>
