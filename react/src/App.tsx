27b90c6f85d846d2cab53ab80e64d87912b702c5
zeeenku <enajjachi.zakariaa@gmail.com>
2025-02-05 02:37:46 +0100

    info faq component added

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -34,6 +34,7 @@ import { Input } from "@/components/ui/input"
 import './App.css'
 import Clock from './Clock'
 import { GamesHistory, Player } from './types'
+import InfoFaq from './InfoFaq'
 
 
 
@@ -308,39 +309,7 @@ const restoreGame = () => {}
 
 
         <div className="w-full lg:w-6/12">
-          <h1 className="text-4xl font-bold text-center">Play Chess with the #1 Chess Timer App!</h1>
-          <h2 className="text-2xl text-semi-brown mt-8 font-bold text-center"> <span>123,765,199</span> 
-          <span className="text-2xl italic text-center"> Games Played</span>
-          </h2>
-          <div className="my-10">
-            <h3 className="text-xl font-semibold text-semi-brown mb-4">Loluptates qui tenetur, doloremque cum delectus mollitia?</h3>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum, assumenda? Delectus, veniam tempora perferendis repellat dolorem aliquam officiis error deserunt sit earum iure. Rerum nulla magni minus facere, repudiandae dolorum.</p>
-          </div>
-
-          <div className="my-10">
-            <h3 className="text-xl font-semibold text-semi-brown mb-4">Loluptates qui tenetur, doloremque cum delectus mollitia?</h3>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum, assumenda? Delectus, veniam tempora perferendis repellat dolorem aliquam officiis error deserunt sit earum iure. Rerum nulla magni minus facere, repudiandae dolorum.</p>
-          </div>
-
-
-          <div className="my-10">
-            <h3 className="text-xl font-semibold text-semi-brown mb-4">Loluptates qui tenetur, doloremque cum delectus mollitia?</h3>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum, assumenda? Delectus, veniam tempora perferendis repellat dolorem aliquam officiis error deserunt sit earum iure. Rerum nulla magni minus facere, repudiandae dolorum.</p>
-          </div>
-
-
-
-          <div className="my-10">
-            <h3 className="text-xl font-semibold text-semi-brown mb-4">Loluptates qui tenetur, doloremque cum delectus mollitia?</h3>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum, assumenda? Delectus, veniam tempora perferendis repellat dolorem aliquam officiis error deserunt sit earum iure. Rerum nulla magni minus facere, repudiandae dolorum.</p>
-          </div>
-
-
-          <div className="my-10">
-            <h3 className="text-xl font-semibold text-semi-brown mb-4">Loluptates qui tenetur, doloremque cum delectus mollitia?</h3>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum, assumenda? Delectus, veniam tempora perferendis repellat dolorem aliquam officiis error deserunt sit earum iure. Rerum nulla magni minus facere, repudiandae dolorum.</p>
-          </div>
-        
+        <InfoFaq/>
         </div>
 </div>
 
---
+++ react/src/InfoFaq.tsx
@@ -0,0 +1,44 @@
+
+
+
+const Info = () => {
+    return (
+        <>
+        <h1 className="text-4xl font-bold text-center">Play Chess with the #1 Chess Timer App!</h1>
+        <h2 className="text-2xl text-semi-brown mt-8 font-bold text-center"> <span>123,765,199</span> 
+        <span className="text-2xl italic text-center"> Games Played</span>
+        </h2>
+        <div className="my-10">
+            <h3 className="text-xl font-semibold text-semi-brown mb-4">Loluptates qui tenetur, doloremque cum delectus mollitia?</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum, assumenda? Delectus, veniam tempora perferendis repellat dolorem aliquam officiis error deserunt sit earum iure. Rerum nulla magni minus facere, repudiandae dolorum.</p>
+        </div>
+
+        <div className="my-10">
+            <h3 className="text-xl font-semibold text-semi-brown mb-4">Loluptates qui tenetur, doloremque cum delectus mollitia?</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum, assumenda? Delectus, veniam tempora perferendis repellat dolorem aliquam officiis error deserunt sit earum iure. Rerum nulla magni minus facere, repudiandae dolorum.</p>
+        </div>
+
+
+        <div className="my-10">
+            <h3 className="text-xl font-semibold text-semi-brown mb-4">Loluptates qui tenetur, doloremque cum delectus mollitia?</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum, assumenda? Delectus, veniam tempora perferendis repellat dolorem aliquam officiis error deserunt sit earum iure. Rerum nulla magni minus facere, repudiandae dolorum.</p>
+        </div>
+
+
+
+        <div className="my-10">
+            <h3 className="text-xl font-semibold text-semi-brown mb-4">Loluptates qui tenetur, doloremque cum delectus mollitia?</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum, assumenda? Delectus, veniam tempora perferendis repellat dolorem aliquam officiis error deserunt sit earum iure. Rerum nulla magni minus facere, repudiandae dolorum.</p>
+        </div>
+
+
+        <div className="my-10">
+            <h3 className="text-xl font-semibold text-semi-brown mb-4">Loluptates qui tenetur, doloremque cum delectus mollitia?</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum, assumenda? Delectus, veniam tempora perferendis repellat dolorem aliquam officiis error deserunt sit earum iure. Rerum nulla magni minus facere, repudiandae dolorum.</p>
+        </div>
+        </>
+    );
+}
+
+
+export default Info;
